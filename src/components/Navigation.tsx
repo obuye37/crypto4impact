ff4de686afb08ccc83bfa0e9f72f260465d070d3
orison-j <obuye37@gmail.com>
2025-02-15 00:26:52 +0100

    first mobile version update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -12,10 +12,11 @@ import { usePathname } from 'next/navigation'
 const Navigation = ():React.JSX.Element => {
     const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false)
     const [closeBtn, setCloseBtn] = useState(false)
+
     const path = usePathname()
     console.log("pathname:" , path)
-    const handleClick = () => {setShowMobileMenu(!showMobileMenu)}
-    const handleCloseBtn = () => {setCloseBtn(true)}
+    const handleClick = () => {setShowMobileMenu(true); setCloseBtn(false)}
+    const handleCloseBtn = () => {setCloseBtn(true); setShowMobileMenu(false)}
   return (
     <header className={styles.header}>
         <Link className={styles.logoLink} href='/' >
@@ -26,24 +27,30 @@ const Navigation = ():React.JSX.Element => {
                 <Link className={styles.navList} href='/about'>ABOUT</Link>
                 <Link className={styles.navList} href='/blog'>BLOG</Link>
                 <Link className={styles.navList} href='/impact'>IMPACT</Link>
-            </div> 
-            <button onClick={()=>handleClick} className={styles.mobileMenuBtn} style={{backgroundColor:'transparent', border:'none'}}>
-                <Menu size={20} style={{color:'whitesmoke'}}/>
-            </button>
+            </div>
+            {
+                !showMobileMenu && 
+                (<button onClick={handleClick} className={styles.mobileMenuBtn} style={{backgroundColor:'transparent', border:'none', cursor:'pointer'}}>
+                    <Menu size={20} style={{color:'whitesmoke'}}/>
+                </button>)
+            } 
+            
         </nav>
         <Link className={styles.navList} href='/donate'>
             DONATE
         </Link>
-        <div className={styles.mobileMenu}>
-            <XCircle size={20} style={{position:'absolute', inset:'15px 25px auto auto', color:'wheat'}}
-                onClick={handleCloseBtn}
-            />
-            
-            <Link className={styles.navList} href='/about'>ABOUT</Link>
-            <Link className={styles.navList} href='/blog'>BLOG</Link>
-            <Link className={styles.navList} href='/impact'>IMPACT</Link>
-            <Link className={styles.navList} href='/donate'>DONATE</Link>
-        </div>
+        {!closeBtn && 
+            <div className={styles.mobileMenu}>
+                <XCircle size={20} style={{position:'absolute', inset:'15px 25px auto auto', color:'wheat', cursor:'pointer'}}
+                    onClick={handleCloseBtn}
+                />
+                <Link className={styles.navList} href='/about'>ABOUT</Link>
+                <Link className={styles.navList} href='/blog'>BLOG</Link>
+                <Link className={styles.navList} href='/impact'>IMPACT</Link>
+                <Link className={styles.navList} href='/donate'>DONATE</Link>
+            </div>
+        }
+        
     </header>
   )
 }
